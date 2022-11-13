1eb8dac51561c3b2e728a764efb6f270eb350aa1
leonardoschutz <118065507+leonardoschutz@users.noreply.github.com>
2022-11-13 20:48:47 -0300

    Add files via upload

---
+++ stocks.R
@@ -0,0 +1,28 @@
+install.packages(c("tidyquant", "ggplot2", "dplyr"))
+
+library(tidyquant)
+library(ggplot2)
+library(dplyr)
+
+multiple_stockes <- tq_get(c("GOOG", "AMZN"),
+                           get = "stock_price",
+                           from = "2022-11-01",
+                           to = "2022-11-30")
+
+
+
+
+
+
+
+
+teste <- data_frame(x = c(1, 2),
+                    y = c(2, 4),
+                    u = c(3, 6),
+                    t = c(4, 10))
+
+
+teste <- teste %>%
+         filter(x > 1)
+  
+  
